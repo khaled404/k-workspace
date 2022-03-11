f267352907a790e7cac9d90e39dd6ab9f8d84d9b
khaled mahmoud <khaled.ma805@gmail.com>
2022-03-11 16:24:04 +0200

    [Added] => [nextjs] blog app

---
+++ tailwind-workspace-preset.js
@@ -0,0 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      height: {
+        max: 'max-content',
+      },
+      colors: {
+        min: '#fd3494',
+        secondary: '#f88d85',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
