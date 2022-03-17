cbda79848389e1b59eaba8890282a9e55bad2ed5
khaled mahmoud <khaled.ma805@gmail.com>
2022-03-17 20:49:43 +0200

    [Added => app] eisenhower next app

---
+++ apps/eisenhower-app/pages/_app.tsx
@@ -0,0 +1,19 @@
+import 'tailwindcss/tailwind.css';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+function CustomApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Welcome to eisenhower-app!</title>
+      </Head>
+
+      <main className="app">
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
+}
+
+export default CustomApp;
---
+++ apps/eisenhower-app/pages/index.tsx
@@ -0,0 +1,12 @@
+import EisenhowerMatrix from '@k-workspace/eisenhower-matrix';
+
+export function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return <EisenhowerMatrix />;
+}
+
+export default Index;
