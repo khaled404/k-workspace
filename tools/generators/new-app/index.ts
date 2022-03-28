b3047520d43b059b4e90bd4b23dcff2277668f04
khaled mahmoud <khaled.ma805@gmail.com>
2022-03-28 17:29:14 +0200

    [update] app generator

--- tools/generators/new-app/index.ts
+++ tools/generators/new-app/index.ts
@@ -1,25 +1,24 @@
 import { Tree, formatFiles, updateJson } from '@nrwl/devkit';
-import { libraryGenerator } from '@nrwl/workspace/generators';
+import { Linter } from '@nrwl/linter';
+import { libraryGenerator } from '@nrwl/react';
 import { join } from 'path';
 
 export default async function (tree: Tree, schema: any) {
   await libraryGenerator(tree, {
     name: schema.name,
     directory: 'apps',
-    importPath: '@apps',
+    importPath: `@apps/${schema.name}`,
+    style: 'none',
+    skipTsConfig: false,
+    skipFormat: false,
+    unitTestRunner: 'jest',
+    linter: Linter.EsLint,
   });
 
   updateJson(tree, join('./libs/apps/projects.json'), (value) => {
     const newJson = {
       ...value,
-      [schema.name]: {
-        version: schema.version,
-        developer: schema.developer,
-        name: schema.name,
-        description: schema.description,
-        published: schema.published,
-        tags: schema.tags,
-      },
+      [schema.name]: { ...schema, imagePath: `/${schema.name}.png` },
     };
 
     return newJson;
