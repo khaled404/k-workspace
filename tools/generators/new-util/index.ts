15f3bc64a7452800c4a13d4f57dd60d02fc2dc40
khaled mahmoud <khaled.ma805@gmail.com>
2022-03-30 17:48:08 +0200

    [Added] utils generator

---
+++ tools/generators/new-util/index.ts
@@ -0,0 +1,40 @@
+import {
+  Tree,
+  formatFiles,
+  generateFiles,
+  joinPathFragments,
+  names,
+} from '@nrwl/devkit';
+
+interface UtilSchemaOptions {
+  name: string;
+}
+
+const exportFilePath = 'libs/utils/src/index.ts';
+
+export default async function (tree: Tree, schema: UtilSchemaOptions) {
+  const fileName = names(schema.name).fileName;
+  generateFiles(
+    tree,
+    joinPathFragments(__dirname, './template'),
+    `./libs/utils/src/${fileName}`,
+    {
+      name: names(schema.name).className,
+      propertyName: names(schema.name).propertyName,
+      fileName: fileName,
+      tmpl: '',
+    }
+  );
+
+  const file = tree.read(exportFilePath);
+
+  const newExports = Buffer.from(
+    `export * from './${fileName}/${fileName}';
+     export * from './${fileName}/${fileName}-types';
+    `
+  );
+
+  tree.write(exportFilePath, Buffer.concat([file, newExports]));
+
+  await formatFiles(tree);
+}
