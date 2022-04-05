f229a91f4bee1d274e4a205283ff6e74f2cdfce6
khaled mahmoud <khaled.ma805@gmail.com>
2022-04-06 00:26:30 +0200

    [Update] libs

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,6 +1,3 @@
 export * from './local-storage/local-storage';
-export * from './local-storage/local-storage-types';
 export * from './class-names/class-names';
-export * from './class-names/class-names-types';
 export * from './root-path/root-path';
-export * from './root-path/root-path-types';
