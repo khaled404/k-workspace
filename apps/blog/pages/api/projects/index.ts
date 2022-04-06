ab756870468e83a8c7f93d5df2e6ca07f3811629
khaled mahmoud <khaled.ma805@gmail.com>
2022-04-06 23:42:25 +0200

    test: server call apis

--- apps/blog/pages/api/projects/index.ts
+++ apps/blog/pages/api/projects/index.ts
@@ -2,7 +2,5 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import projects from '../data/projects.json';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const data = JSON.stringify(projects);
-
-  res.status(200).json(data);
+  res.status(200).json(projects);
 }
