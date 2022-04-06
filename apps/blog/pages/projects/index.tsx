141a9d83abf29128528a909244bf87a1aa053b3e
khaled mahmoud <khaled.ma805@gmail.com>
2022-04-06 02:52:49 +0200

    test: server call apis

--- apps/blog/pages/projects/index.tsx
+++ apps/blog/pages/projects/index.tsx
@@ -1,19 +1,28 @@
 import { ProjectBox, TProject } from '@k-workspace/shared/ui';
 import { httpDriver } from '@k-workspace/utils';
 import { GetStaticProps } from 'next';
+import { useEffect } from 'react';
 
 export interface ProjectsProps {
   projects: TProject[];
 }
 
 export function Projects({ projects }: ProjectsProps) {
+  const loadData = async () => {
+    const data = await httpDriver<TProject[]>('projects');
+    console.log(data);
+  };
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return <ProjectBox projects={projects} />;
 }
 
 export default Projects;
 
 export const getStaticProps: GetStaticProps<ProjectsProps> = async () => {
-  const data = await httpDriver<TProject[]>('projects');
+  const data = (await httpDriver<TProject[]>('projects')) || [];
   return {
     props: {
       projects: data,
