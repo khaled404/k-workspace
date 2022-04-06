2f06d88a68f355128a1c049eb63142acc5cfcb90
khaled mahmoud <khaled.ma805@gmail.com>
2022-04-06 23:26:17 +0200

    test: server call apis

--- apps/blog/pages/api/projects/index.ts
+++ apps/blog/pages/api/projects/index.ts
@@ -1,7 +1,5 @@
-import { rootPath } from '@k-workspace/utils';
-import { readFileSync } from 'fs';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import projects from '../../../../../data/projects.json';
+import projects from '../data/projects.json';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json(projects);
