774e8a4fb6ac827ed98ab9abe919d329af6313bb
khaled mahmoud <khaled.ma805@gmail.com>
2022-04-06 03:22:35 +0200

    test: server call apis

--- apps/blog/pages/index.tsx
+++ apps/blog/pages/index.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 
 export function Index() {
   const loadData = async () => {
-    const data = await httpDriver('api');
+    const data = await httpDriver();
     console.log(data);
   };
   useEffect(() => {
