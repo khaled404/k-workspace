ad77ff5ea0d8342839da927e043a7af34e3b9069
khaled mahmoud <khaled.ma805@gmail.com>
2022-04-09 23:47:01 +0200

    test: deploy error

--- apps/blog/pages/api/index.ts
+++ apps/blog/pages/api/index.ts
@@ -1,9 +1,5 @@
-import { FooterProps, NavbarProps } from '@k-workspace/shared/ui';
-import { getFileParser } from '../../utils/data-provider/data-provider';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const navbar = getFileParser<NavbarProps>('navbar');
-  const footer = getFileParser<FooterProps>('footer');
-  res.status(200).json({ navbar, footer });
+  res.status(200).json({ hi: 'hi' });
 }
