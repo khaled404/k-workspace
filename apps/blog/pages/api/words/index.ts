0f8dd2ba07b390dd559db7249a7630e66cce640d
khaled mahmoud <khaled.ma805@gmail.com>
2022-06-05 22:53:14 +0200

    fix: server error

--- apps/blog/pages/api/words/index.ts
+++ apps/blog/pages/api/words/index.ts
@@ -1,16 +1,23 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { addNewWord, getAllWords } from '@k-workspace/json-data-provider';
+// import { addNewWord, getAllWords } from '@k-workspace/json-data-provider';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import fs from 'fs';
+import { join } from 'path';
 
-export default function getWords(req: NextApiRequest, res: NextApiResponse) {
-  const { method, body } = req;
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // const { method, body } = req;
   try {
-    if (method === 'POST') {
-      const newWord = JSON.parse(body);
-      addNewWord(newWord);
-      return res.status(200).json({ massage: 'added successfully' });
-    }
-    const data = getAllWords();
+    // if (method === 'POST') {
+    //   const newWord = JSON.parse(body);
+    //   addNewWord(newWord);
+    //   return res.status(200).json({ massage: 'added successfully' });
+    // }
+
+    const file: any = fs.readFileSync('/data/words/words.json');
+    const data = JSON.parse(file);
 
     res.status(200).json({ data });
   } catch (error) {
