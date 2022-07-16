232fd1538bf502fe220e66e6ce7c2f692f87e5c9
khaled404 <khaled.ma805@gmail.com>
2022-07-16 16:18:33 +0200

    fix: nx json error

--- apps/blog/pages/components/page.tsx
+++ apps/blog/pages/components/page.tsx
@@ -1,19 +1,18 @@
 import type { FC } from '@k-workspace/types';
-import { Footer, LoginAndRegister, Navbar } from '@k-workspace/shared/ui';
-import { useModal } from '@k-workspace/shared/components';
+import { Footer, Navbar } from '@k-workspace/shared/ui';
 const Page = ({ children }: FC) => {
-  const { newModal } = useModal();
-  const openModal = (type: string) => {
-    newModal({
-      type: 'custom',
-      renderCustom: (setOpen) => (
-        <LoginAndRegister type={type} setOpen={setOpen} />
-      ),
-    });
-  };
+  // const { newModal } = useModal();
+  // const openModal = (type: string) => {
+  //   newModal({
+  //     type: 'custom',
+  //     renderCustom: (setOpen) => (
+  //       <LoginAndRegister type={type} setOpen={setOpen} />
+  //     ),
+  //   });
+  // };
   return (
     <div>
-      <Navbar onChange={openModal} />
+      <Navbar />
       {children}
       <Footer />
     </div>
